b6372c442423308fdb77c2f35e4ac9498552818a
sukumar kandipati <sukumar.kandipati@ue-germany.de>
2026-07-01 16:36:32 +0200

    AutoDeploy.AI update app.py

--- app.py
+++ app.py
@@ -1,22 +1,9 @@
 from flask import Flask
-from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 from routes import init_routes
-import logging
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-CORS(app)
-db = SQLAlchemy(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
 
 init_routes(app)
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
