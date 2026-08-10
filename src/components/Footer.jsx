674d2ac0560028627f7ac7fc936b5e2889edbdb4
sukumar kandipati <sukumar.kandipati@ue-germany.de>
2026-08-10 23:19:42 +0200

    AutoDeploy.AI update src/components/Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2023 Cloud Productivity Platform. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
