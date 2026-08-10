8a8948bcab0ba0cf1bf564f0322bba1b0e76e285
sukumar kandipati <sukumar.kandipati@ue-germany.de>
2026-08-10 23:19:37 +0200

    AutoDeploy.AI update src/components/Workflow.jsx

---
+++ src/components/Workflow.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Workflow() {
+  return (
+    <section className="workflow">
+      <h2>How It Works</h2>
+      <ol>
+        <li>Sign up and create an account</li>
+        <li>Set up your workspace</li>
+        <li>Invite your team</li>
+        <li>Start collaborating</li>
+      </ol>
+    </section>
+  );
+}
+
+export default Workflow;
